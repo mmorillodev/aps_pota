63a8ecc5ce56d14c2c4e05b554181a2330069e8d
mmorillo <matheus.morillo2000@gmail.com>
2019-09-18 22:17:40 -0300

    class and methods visibility modified

--- src/StopWatch.java
+++ src/StopWatch.java
@@ -1,18 +1,18 @@
-public class StopWatch {
+class StopWatch {
 
     private double resultant_time;
     private double initial;
 
-    public void start() {
+    void start() {
         resultant_time = 0;
         initial = System.nanoTime();
     }
 
-    public double stop() {
+    double stop() {
         return resultant_time = System.nanoTime() - initial;
     }
 
-    public double getResultant(boolean milli) {
+    double getResultant(boolean milli) {
         return (milli ? resultant_time/1e3 : resultant_time);
     }
 }
