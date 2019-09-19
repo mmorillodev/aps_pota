f7098e378f0d7e5edc2c66e56f4142bf84035822
mmorillo <matheus.morillo@nescara.com>
2019-09-19 14:21:42 -0300

    grammar fixed

--- src/Tests.java
+++ src/Tests.java
@@ -2,7 +2,7 @@ public class Tests {
     public static void main(String[] args) {
 //        StopWatch stopWatch = new StopWatch();
         int[] arr = {9, 5, 4, 1, 8, 3, 2, 7, 6};
-        int[] correntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] correctArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9;
         String errorMsg = "";
 
@@ -16,31 +16,31 @@ public class Tests {
         Arrays.bucketSort(arr7 = arr.clone());
         Arrays.radixSort(arr8 = arr.clone());
 
-        if(!Arrays.areEquals(arr1, correntArray)) {
+        if(!Arrays.areEquals(arr1, correctArray)) {
             errorMsg += "Bubble";
         }
-        if(!Arrays.areEquals(arr2, correntArray)) {
+        if(!Arrays.areEquals(arr2, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Selection";
         }
-        if(!Arrays.areEquals(arr3, correntArray)) {
+        if(!Arrays.areEquals(arr3, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Insertion";
         }
-        if(!Arrays.areEquals(arr4, correntArray)) {
+        if(!Arrays.areEquals(arr4, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Merge";
         }
-        if(!Arrays.areEquals(arr5, correntArray)) {
+        if(!Arrays.areEquals(arr5, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Quick";
         }
-        if(!Arrays.areEquals(arr6, correntArray)) {
+        if(!Arrays.areEquals(arr6, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Count";
         }
-        if(!Arrays.areEquals(arr7, correntArray)) {
+        if(!Arrays.areEquals(arr7, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Bucket";
         }
-        if(!Arrays.areEquals(arr8, correntArray)) {
+        if(!Arrays.areEquals(arr8, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Radix";
         }
-        if(!Arrays.areEquals(arr9, correntArray)) {
+        if(!Arrays.areEquals(arr9, correctArray)) {
             errorMsg += (errorMsg.length() > 0 ? ", " : "") + "Heap";
         }
 
