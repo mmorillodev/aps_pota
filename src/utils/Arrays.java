0c3fe04cb8c95dfbf490837af1b15ce89604212f
mmorillo <matheus.morillo@nescara.com>
2019-10-29 10:52:41 -0300

    c-style array declaration replaced

--- src/utils/Arrays.java
+++ src/utils/Arrays.java
@@ -86,7 +86,7 @@ public class Arrays {
         }
     }
 
-    private static void heapify(int arr[], int n, int i) {
+    private static void heapify(int[] arr, int n, int i) {
         int largest = i;
         int left = 2*i + 1;
         int right = 2*i + 2;
