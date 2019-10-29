cbb084434824674beda67df57d2cf9dd8c4f4c20
matheus morillo <matheus.morillo2000@gmail.com>
2019-10-29 00:25:49 +0000

    TestManager.java edited online with Bitbucket

--- src/utils/TestManager.java
+++ src/utils/TestManager.java
@@ -27,7 +27,7 @@ public class TestManager {
 
         StopWatch stopWatch = new StopWatch();
         
-        int[] arr = Arrats.getIntArray(this.arrSize);
+        int[] arr = Arrays.getIntArray(this.arrSize);
 
         stopWatch.start();
         Arrays.bubbleSort(copyArr(arr));
@@ -123,7 +123,7 @@ public class TestManager {
     }
 
     public int size() {
-        return arr.length;
+        return arrSize;
     }
 
     public String toString() {
